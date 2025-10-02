f57e7e6097511f5c82281e938cf94baebfaad7b9
hoanglephibao.1212@gmail.com <huynhthiainhi2636@gmail.com>
2025-10-02 11:06:00 +0700

    update en ez

---
+++ js/i18n.js
@@ -0,0 +1,18 @@
+// Thêm các key mới này vào i18n.cs và i18n.en
+const extraDict = {
+  cs: {
+  "nav.home": "Home",
+  "nav.shop": "E-shop",
+  "nav.categories": "Kategorie",
+  "nav.about": "O nás",
+  "nav.contact": "Kontakt"
+  },
+  en: {
+  "nav.home": "Home",
+  "nav.shop": "E-Shop",
+  "nav.categories": "Categories",
+  "nav.about": "About us",
+  "nav.contact": "Contact"
+  }
+};
+
