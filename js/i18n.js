8c15b5cd298249cb2cbf90d4d06da34a3c9f5d5d
hoanglephibao.1212@gmail.com <huynhthiainhi2636@gmail.com>
2025-10-16 18:01:05 +0700

    cap nhat phan tu lanh

--- js/i18n.js
+++ js/i18n.js
@@ -203,11 +203,19 @@
   ,"section.coolingtables": "Nerezové chladicí a mrazicí stoly",
   "shockfreezers.text1": "V naší nabídce najdete Šokové zchlazovače a zmrazovače, které výborně slouží pro zefektivnění výrobního procesu stravovacích provozů, kdy vyrobená jídla lze šokově zchladit nebo zmrazit a uchovat je tak pro pozdější výdej.",
   "shockfreezers.text2": "Šokové zchlazování až zmrazování probíhá podle předem stanovené zchlazovací křivky, aby při procesu byl maximálně potlačen výskyt a množení mikroorganismů."
-,"section.accessories": "Příslušenství ke stolům"
+,"section.accessories": "Příslušenství ke stolům",
+"section.chladici": "Chladicí Nástavby",
+"section.podestavby": "Chladicí Podestavby",
+"section.nerezove_chladici": "NEREZOVÉ CHLADICÍ",    
+"coolingtables.text1": "V naší nabídce najdete různé typy chladicích stolů a saladet v nerezovém provedení. Všechny typy vynikají svou vysokou účinností chlazení i při vysokých okolních teplotách."
         },
 
         en: {
              "page.gastro.title": "Gastro and Kitchen",
+             "section.nerezove_chladici": "Stainless steel cooling cabinets",   
+             "section.podestavby":"Cooling substructures",
+             "section.chladici": "Cooling extensions",
+              "coolingtables.text1": "In our offer you will find various types of cooling tables and saladettes made of stainless steel. All models excel in high cooling efficiency even at elevated ambient temperatures.",
              "section.accessories": "Table Accessories",
                "shockfreezers.text1": "In our offer you will find blast chillers and freezers that efficiently improve the production process of catering facilities, allowing prepared meals to be rapidly cooled or frozen and preserved for later serving.",
   "shockfreezers.text2": "Blast chilling and freezing take place according to a predefined cooling curve to minimize the occurrence and growth of microorganisms during the process.",
